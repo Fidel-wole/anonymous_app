f5f4cd3997612bae951159d9c398a8ac05f7c5b1
Adewole Adewoye <adewoyeadedayo10@gmail.com>
2023-09-25 12:55:24 +0100

    addition of backend

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,18 @@ import Header from "./components/header/Header";
 import Messages from "./pages/messages/Messages";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Message from "./pages/message/Message";
+import SignInPage from "./pages/auth/login/Login";
 
 function App() {
   return (
 <>
 
-<BrowserRouter basename={process.env.PUBLIC_URL}>
+<BrowserRouter basename={process.env.PUBLIC_URL}> 
 <Header/>
+
   <Routes>
-    <Route path='/' exact Component={Home} />
+    <Route path='/' exact Component={SignInPage} />
+    <Route path='/home' exact Component={Home} />
     <Route path="/messages" exact Component={Messages} />
     <Route path="/message/:id" exact Component={Message} />
   </Routes>
---
+++ src/pages/auth/login/Login.jsx
@@ -0,0 +1,77 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { useState } from 'react';
+import './login.css'
+
+export default function SignInPage() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+  
+    const handleUsernameChange = (e) => {
+      setUsername(e.target.value);
+    };
+  
+    const handlePasswordChange = (e) => {
+      setPassword(e.target.value);
+    };
+  
+    const handleLogin = async (e) => {
+      e.preventDefault();
+  
+      const loginData = {
+        username: username,
+        password: password,
+      };
+  
+      try {
+        const response = await fetch('http://localhost:8000/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(loginData),
+        });
+  
+        if (response.ok) {
+          // Authentication successful
+          const data = await response.json();
+          window.location.href="/home";
+          // You can handle the successful login response here (e.g., set user session, redirect, etc.)
+        } else {
+          // Authentication failed
+          console.error('Login failed');
+
+        }
+      } catch (error) {
+        console.error('Error occurred during login:', error);
+      }
+    };
+  
+    return (
+        <div className="text-center m-5-auto">
+            <div>
+           
+            <h2>Sign in to us</h2>
+            <form onSubmit={handleLogin}>
+                <p>
+                    <label>Username or email address</label><br/>
+                    <input type="text" value={username} onChange={handleUsernameChange} required />
+                </p>
+                <p>
+                    <label>Password</label>
+                    <Link to="/forget-password"><label className="right-label">Forget password?</label></Link>
+                    <br/>
+                    <input type="password" value={password} onChange={handlePasswordChange} required />
+                </p>
+                <p>
+                    <button id="sub_btn" type="submit">Login</button>
+                </p>
+            </form>
+            <footer>
+                <p>First time? <Link to="/register">Create an account</Link>.</p>
+                <p><Link to="/">Back to Homepage</Link>.</p>
+            </footer>
+        </div>
+        </div>
+    )
+}
